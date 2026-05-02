39e875bed694eb6817234424a62879d729b5d6f4
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-05-02 01:26:26 -0400

    Created FC, called a usestate hook for state variable for the components, started with useeffect

--- src/components/AdvancedCounters.tsx
+++ src/components/AdvancedCounters.tsx
@@ -1,2 +1,33 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
+// Making Advance counter a React.FC which is a (TypeScript Type) for a Functional Componenet
+export const AdvancedCounter: React.FC = () => {
+//count - current value, setCount - function to update it
+    const [count, setCount] = useState<number>(() => {
+        const saved = localStorage.getItem('count');
+        return saved ? Number(saved) : 0;
 
+    });
+// Declaring state (react hooks) as the const
+const [step, setStep] = useState<number>(1);
+const [history, setHistory] = useState<number[]>([]);
+
+// To track the history
+
+useEffect(() => {
+    setHistory(prev => [...prev,count]);
+
+}, [count]);
+
+useEffect(() => {
+let isCancelled = false;
+
+const saveToStorage = () => {
+    if(!isCancelled) {
+        localStorage.setItem('count', count.toString());
+    }
+
+};
+
+)
+
+}
